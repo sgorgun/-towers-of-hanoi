3d0a3057a948dfe303ace59039f6fcb59cf64c52
sgorgun <sergei.gorgun@gmail.com>
2023-10-11 20:39:30 +0300

    Ready with pragma

--- TowersOfHanoi/TowerOfHanoi.cs
+++ TowersOfHanoi/TowerOfHanoi.cs
@@ -21,9 +21,13 @@
         {
             if (totalDisks > 0)
             {
+#pragma warning disable S2234 // Arguments should be passed in the same order as the method parameters
                 this.MoveDisks(totalDisks - 1, sourceTower, tempTower, targetTower);
+#pragma warning restore S2234 // Arguments should be passed in the same order as the method parameters
                 Console.WriteLine($"Move disk {totalDisks} from tower {sourceTower} to tower {targetTower}");
+#pragma warning disable S2234 // Arguments should be passed in the same order as the method parameters
                 this.MoveDisks(totalDisks - 1, tempTower, targetTower, sourceTower);
+#pragma warning restore S2234 // Arguments should be passed in the same order as the method parameters
             }
         }
     }
